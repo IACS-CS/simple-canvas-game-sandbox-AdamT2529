55bc1483ac6c55564434c1e5c025bd91dc8db862
AdamT2529 <profileytwow1@gmail.com>
2025-11-10 14:27:10 +0000

    Need help with fixing code going to ask Mr. Hinkle next class

--- main.js
+++ main.js
@@ -6,10 +6,20 @@ let gi = new GameInterface();
 
 gi.addDrawing(
   function ({ ctx, width, height, elapsed }) {
-    // Your drawing code here...
-    // (see demo.js for fuller example)
-  }
-)
+    ctx.beginPath();
+    ctx.strokeStyle = "rgba(1, 228, 249, 1)";
+    ctx.lineWidth = 5;
+    let x = 30 + ((elapsed / 10)) % width /2); 
+    let y = 60 + ((elapsed /10)) % height /2);
+for (let i = 0; i < 60; i++) {
+  ctx.moveTo(x, y);
+  ctx.lineTo(x, y + 10);
+  ctx.lineTo(x + 20, y + 10);
+  x = x + 20;
+  y = y + 10;
+}
+
+ctx.stroke();
 
 gi.run();
 
