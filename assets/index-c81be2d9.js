03f49cc374cca3de9c9673fc593a76c4948097c9
AdamT2529 <AdamT2529@users.noreply.github.com>
2025-11-12 17:30:14 +0000

    deploy: 719119bb325d06d5114e843bb457c5fb7ba650c1

--- assets/index-c81be2d9.js
+++ assets/index-c81be2d9.js
@@ -980,12 +980,38 @@ class T extends p {
 
 let gi = new T();
 
-gi.addDrawing(
-  function ({ ctx, width, height, elapsed }) {
-    // Your drawing code here...
-    // (see demo.js for fuller example)
-  }
-);
+let top = 0;
+let left = 0;
 
+gi.addDrawing(function ({ ctx, width, height, stepTime }) {
+  ctx.strokeStyle = 'rgba(0, 247, 255, 1)';
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  let x = left;
+  let y = top;
+  // Make a staircase pattern across the screen
+  for (let i = 0; i < 60; i++) {
+    ctx.moveTo(x, y);
+    ctx.lineTo(x, y + 10);
+    ctx.lineTo(x + 20, y + 10);
+    x += 20;
+
+    y += 10;
+  }
+
+  ctx.stroke();
+  // Update staircase position for next frame
+  top += stepTime * 0.05;
+  left += stepTime * 0.1;
+
+  if(top > height) {
+    top = 0;
+  }
+  if (left > width) {
+    left = 0;
+  }
+
+});
+ 
 gi.run();
-//# sourceMappingURL=index-d7b345db.js.map
+//# sourceMappingURL=index-c81be2d9.js.map
